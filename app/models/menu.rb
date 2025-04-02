dc617290d0b2a2799bed8bc8f64add6ec9eb675a
George Z Pittas <you@youraddress.com>
2025-04-02 08:40:16 -0400

    enable ability to delete menus

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 # app/models/menu.rb
 class Menu < ApplicationRecord
-  has_many :menu_categories, -> { order(position: :asc) }
+  has_many :menu_categories, -> { order(position: :asc) }, dependent: :destroy
   has_many :categories, through: :menu_categories
   has_many :menu_items, through: :menu_categories
   has_many :items, through: :menu_items
--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -2,7 +2,7 @@
 class MenuCategory < ApplicationRecord
   belongs_to :menu
   belongs_to :category
-  has_many :menu_items, -> { order(position: :asc) }
+  has_many :menu_items, -> { order(position: :asc) }, dependent: :destroy
   
   acts_as_list scope: :menu
 end
